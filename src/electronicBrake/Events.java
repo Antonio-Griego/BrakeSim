9644df8553dd27b4e7b9163f535d799e8260e0eb
Antonio Griego <deano505@unm.edu>
2018-03-24 00:14:09 -0600

    Added dummy Events class in order to work on EBS class
    This dummy class was added so that development could continue on
    the EBS class until the full version of the Events class was
    implemented and uploaded to replace this class.

---
+++ src/electronicBrake/Events.java
@@ -0,0 +1,18 @@
+package electronicBrake;
+
+/**
+ * Created by Antonio on 3/23/18.
+ *
+ * Dummy class so that I can continue working on the Controller (EBS) module.
+ * Please replace this class with the working module.
+ */
+public class Events
+{
+  BrakeControl brakeControl;
+  PressHandler pressHandler;
+  GearHandler gearHandler;
+
+  public Events()
+  {
+  }
+}
