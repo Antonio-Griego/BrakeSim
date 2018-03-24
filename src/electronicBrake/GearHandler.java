975c94625ec05f192d278457b75f881615cb7d09
Antonio Griego <deano505@unm.edu>
2018-03-24 03:27:55 -0600

    added dummy class for GearHandler
    Added a dummy class for GearHandler so that the code compiles
    and runs, albeit incorrectly until a correct GearHandler is
    implemented.

---
+++ src/electronicBrake/GearHandler.java
@@ -0,0 +1,21 @@
+package electronicBrake;
+
+import Car.Gear;
+import VirtualDevices.VehicleElectronics;
+
+/**
+ * Created by antonio on 3/24/18.
+ *
+ * Dummy class, please replace me.
+ */
+public class GearHandler
+{
+  public GearHandler(VehicleElectronics vehicleElectronics)
+  {
+  }
+
+  public Gear getGear()
+  {
+    return Gear.DRIVE;
+  }
+}
